0a21010565fe7016a03cb19bb851ebec0b889a45
Sparno1179 <39694862+Sparno1179@users.noreply.github.com>
2018-11-08 19:26:51 +0900

    v1.0 計測可能

--- Arduino/ITL/apps.ino
+++ Arduino/ITL/apps.ino
@@ -3,12 +3,8 @@
 #include <string.h>
 #include "utility/MPU9250.h"
 #include "utility/quaternionFilters.h"
+#include <M5StackSAM.h>
 
-// ↓いつの間にこれなしでもVScodeがエラー出さなくなりました
-//
-// VScodeでエラーを出さないための記述。Arduinoでコンパイル時はコメントアウトすること。
-// #include <M5StackSAM.h>
-// M5SAM MyMenu;
 
 // LCDの中央座標を示す。Y座標は3列分用意。
 #define LCDcenterX 160
@@ -43,8 +39,13 @@ void appCsvFileCount(){
     M5.update();
   }
   MyMenu.windowClr();
-  M5.Lcd.drawCentreString(F("File count"),LCDcenterX,LCDcenterY1,2);
-  M5.Lcd.drawCentreString(String(fileCount(SD, "/walk", 0))+"  CSVs in SD card",LCDcenterX,LCDcenterY2,2);   
+  M5.Lcd.drawString(String(fileCount(SD, "/acc/walk", 0))+"  CSVs in WALK",LCDcenterX-50,LCDcenterY1-50,2);
+  M5.Lcd.drawString(String(fileCount(SD, "/acc/stand", 0))+"  CSVs in STAND",LCDcenterX-50,LCDcenterY1-30,2);
+  M5.Lcd.drawString(String(fileCount(SD, "/acc/sit", 0))+"  CSVs in SIT",LCDcenterX-50,LCDcenterY1-10,2);
+  M5.Lcd.drawString(String(fileCount(SD, "/acc/stdown", 0))+"  CSVs in STAIR_DOWN",LCDcenterX-50,LCDcenterY2-10,2);
+  M5.Lcd.drawString(String(fileCount(SD, "/acc/stup", 0))+"  CSVs in STAIR_UP",LCDcenterX-50,LCDcenterY3-10,2);
+  M5.Lcd.drawString(String(fileCount(SD, "/acc/lying", 0))+"  CSVs in LYING",LCDcenterX-50,LCDcenterY3+10,2);
+  M5.Lcd.drawString(String(fileCount(SD, "/acc/vehicle", 0))+"  CSVs in VEHICLE",LCDcenterX-50,LCDcenterY3+30,2);
   while(!M5.BtnB.wasPressed()){
     M5.update();
   }
@@ -77,7 +78,7 @@ void appDrawAccGyro() {
     IMU.initMPU9250();
     getAcc(&IMU, &sensorData);
 
-    if(abs(abs(sensorData.accX) - abs(sensorData.accY)) < 30) {
+    if(abs(sensorData.accX - sensorData.accY) < 70) {
       break;
     }
   }
@@ -166,119 +167,165 @@ volatile bool buffSaveFlg = false;
  * 60秒計測ー＞新しいファイルで保存ー＞60秒計測.......
  */
 void appAccTimer() {
-    MyMenu.drawAppMenu(F("Save Acc by Timer"),F(""),F("EXIT"),F(""));
+  MyMenu.drawAppMenu(F("Save Acc by Timer"),F(""),F("EXIT"),F(""));
   
-    while(M5.BtnB.wasPressed()){
-      M5.update();
-    }
+  while(M5.BtnB.wasPressed()){
+    M5.update();
+  }
   
-    // 加速度・ジャイロセンサの宣言・初期化
-    M5.Lcd.drawCentreString("Loading...", LCDcenterX, LCDcenterY2, 2);
-    delay(1000);
-    static MPU9250 IMU;
+  // 加速度・ジャイロセンサの宣言・初期化
+  M5.Lcd.drawCentreString("Loading...", LCDcenterX, LCDcenterY2, 2);
+  delay(1000);
+  MPU9250 IMU;
+  struct sensorData sensorData = {0, 0, 0, 0, 0, 0};
+
+  while(true) {
     IMU.calibrateMPU9250(IMU.gyroBias, IMU.accelBias);
     IMU.initMPU9250();
-  
-    MyMenu.drawAppMenu(F("Save acc and gyro"),F("START"),F("EXIT"),F(""));
-    MyMenu.windowClr();
-    M5.Lcd.drawCentreString("Press START to start measure", LCDcenterX, LCDcenterY2, 2);
+    getAcc(&IMU, &sensorData);
 
-    sdBuff = (struct sensorData*)malloc( sizeof(struct sensorData)*4096); //長さ4096を確保 ＊最後にfree(sdBuff)
-    while(!M5.BtnB.wasPressed()){
-      if(M5.BtnA.wasPressed()) {
-        MyMenu.drawAppMenu(F("keisoku chu desu"),F(""),F("EXIT"),F("STOP"));
-
-        // 16ミリ秒ごと(62.5Hz)にセンサーリード
-        tickerSensor.attach_ms<MPU9250*>(16, _readSensor, &IMU);
-        // 1秒ごとに経過時間を表示
-        tickerShowTime.attach_ms(1000, _showElapsedTime);
-        // 60秒ごとにフラグ（buffSaveFlg）を立てる
-        tickerWriteData.attach_ms(10000, _buffSave); 
-
-        while(!M5.BtnC.wasPressed()) {
-          M5.update();
-          if(buffSaveFlg) {
-            Serial.println("buffSaveFlg setted!");
-            //タイマーを止める
-            tickerSensor.detach();
-            tickerShowTime.detach();
-            tickerWriteData.detach();
-            Serial.println("All ticker detached!");
-
-            //ファイル作成
-            Serial.println("making csv file...");
-            saveIndex = fileCount(SD, "/walk", 0);
-            Serial.print("saveIndex = "); Serial.println(saveIndex);
-            char fileName[16] = {};
-            sprintf(fileName, "/walk/%d.csv", saveIndex);
-            Serial.print("fileName = "); Serial.println(fileName);
-            File file = SD.open(fileName, FILE_WRITE);
-            Serial.print("Opened file = "); Serial.println(file);
-            
-            
-            //Serial.println(fileName);
-            //ファイルが開けないとき
-            if(!file) {
-                MyMenu.windowClr();
-                M5.Lcd.drawCentreString("SD not found?Plz Insert SD and reboot", LCDcenterX, LCDcenterY2, 2);
-                Serial.println("SD not found?Plz Insert SD and reboot");
-                tickerSensor.detach();
-                tickerShowTime.detach();
-                tickerWriteData.detach();
-                break;
-            }
-            Serial.println("opened csv file successfully!");
-            //バッファを全て書き込み
-            clearY1();
-            Serial.println("CSV writing...");
-            M5.Lcd.drawCentreString("CSV Writing...", LCDcenterX, LCDcenterY1, 2);
-            for(int i = 0; i < buffPointer; i++) {
-              char buf[64];
-              sprintf(buf, "%d, %d, %d, %d, %d, %d", sdBuff[i].accX, sdBuff[i].accY,sdBuff[i].accZ, sdBuff[i].gyroX, sdBuff[i].gyroY, sdBuff[i].gyroZ);
-              file.println(buf);
-            }
-            
-            file.close();
-            Serial.println("File closed!");
-
-            clearY1();
-            M5.Lcd.drawCentreString("Write Complete!", LCDcenterX, LCDcenterY3, 2);
-            
-            //バッファ初期化
-            buffPointer = 0;
-            buffSaveFlg = false;
-            
-            // 計測開始
-            tickerSensor.attach_ms<MPU9250*>(16,_readSensor, &IMU);
-            tickerShowTime.attach_ms(1000, _showElapsedTime);
-            tickerWriteData.attach_ms(10000, _buffSave); 
-            M5.Lcd.drawCentreString("Measurement Start!", LCDcenterX, LCDcenterY1, 2);
-            Serial.println("All ticker attached!");
-          }
-          delay(100);
-        }
-        
-        //タイマーを止める
-        tickerSensor.detach();
-        tickerShowTime.detach();
-        tickerWriteData.detach();
-        //バッファ初期化
-        buffPointer = 0;
-        elapsedTime = 0;
-        buffSaveFlg = false;
-
-        MyMenu.windowClr();
-
-        // 加速度・ジャイロ表示ループ終了
-        MyMenu.drawAppMenu(F("Save acc and gyro"),F("START"),F("EXIT"),F(""));
-        MyMenu.windowClr();
-        M5.Lcd.drawCentreString("Press START to start measure", LCDcenterX, LCDcenterY2, 2);
-      }
-      M5.update();
+    if(abs(sensorData.accX - sensorData.accY) < 70) {
+      break;
     }
-    free(sdBuff);
-    MyMenu.show();
+  }
+  
+  MyMenu.drawAppMenu(F("Save acc and gyro"),F("OK"),F("EXIT"),F("NEXT"));
+  MyMenu.windowClr();
+
+  char movementList[7][20] = {"walk", "stand", "sit", "stdown", "stup", "lying", "vehicle"};
+  int movementID = -1;
+  MyMenu.clearList();
+  MyMenu.setListCaption("Movement Type");
+  MyMenu.addList("WALK");
+  MyMenu.addList("STAND");
+  MyMenu.addList("SIT");
+  MyMenu.addList("STAIR_DOWN");
+  MyMenu.addList("STAIR_UP");
+  MyMenu.addList("LYING");
+  MyMenu.addList("VEHICLE");
+  MyMenu.showList();
+
+  sdBuff = (struct sensorData*)malloc( sizeof(struct sensorData)*4096); //長さ4096を確保 ＊最後にfree(sdBuff)
+
+
+  // メインループ
+  while(!M5.BtnB.wasPressed()){
+    
+    // Cボタンでリスト送り
+    if(M5.BtnC.wasPressed()) {
+      MyMenu.drawAppMenu(F("save acc and gyro"),F("OK"),F("EXIT"),F("NEXT"));
+      MyMenu.nextList();
+    }
+    
+    // Aボタンを押すと計測スタート
+    if(M5.BtnA.wasPressed()) {
+
+      // 行動IDを取得
+      for(int i=-1; i < MyMenu.getListID(); i++) {
+        movementID = i + 1;
+      }
+
+      MyMenu.drawAppMenu(("SAVE "+String(movementList[movementID])),F(""),F("EXIT"),F("STOP"));
+
+      // 計測開始
+      M5.Lcd.drawCentreString("Waiting...", LCDcenterX, LCDcenterY2, 2);
+      delay(5000);
+      MyMenu.windowClr();
+      // 16ミリ秒ごと(62.5Hz)にセンサーリード
+      tickerSensor.attach_ms<MPU9250*>(16, _readSensor, &IMU);
+      // 1秒ごとに経過時間を表示
+      tickerShowTime.attach_ms(1000, _showElapsedTime);
+      // 60秒ごとにフラグ（buffSaveFlg）を立てる
+      tickerWriteData.attach_ms(30000, _buffSave); 
+
+      while(!M5.BtnC.wasPressed()) {
+        M5.update();
+        if(buffSaveFlg) {
+          Serial.println("buffSaveFlg setted!");
+          //タイマーを止める
+          tickerSensor.detach();
+          tickerShowTime.detach();
+          tickerWriteData.detach();
+          Serial.println("All ticker detached!");
+
+          // movementIDより、保存先パスの設定
+          char fileName[25] = {};
+          sprintf(fileName, "/acc/%s", movementList[movementID]);
+          //ファイル作成
+          Serial.println("making csv file...");
+          saveIndex = fileCount(SD, fileName, 0);
+          Serial.print("saveIndex = "); Serial.println(saveIndex);
+          sprintf(fileName, "/acc/%s/%d.csv", movementList[movementID], saveIndex);
+          Serial.print("fileName = "); Serial.println(fileName);
+          File file = SD.open(fileName, FILE_WRITE);
+          Serial.print("Opened file = "); Serial.println(file);
+          
+          
+          //Serial.println(fileName);
+          //ファイルが開けないとき
+          if(!file) {
+              MyMenu.windowClr();
+              M5.Lcd.drawCentreString("SD not found?Plz Insert SD and reboot", LCDcenterX, LCDcenterY2, 2);
+              Serial.println("SD not found?Plz Insert SD and reboot");
+              tickerSensor.detach();
+              tickerShowTime.detach();
+              tickerWriteData.detach();
+              break;
+          }
+          Serial.println("opened csv file successfully!");
+          //バッファを全て書き込み
+          clearLCDY1();
+          Serial.println("CSV writing...");
+          M5.Lcd.drawCentreString("CSV Writing...", LCDcenterX, LCDcenterY1, 2);
+          for(int i = 0; i < buffPointer; i++) {
+            char buf[64];
+            sprintf(buf, "%d, %d, %d, %d, %d, %d", sdBuff[i].accX, sdBuff[i].accY,sdBuff[i].accZ, sdBuff[i].gyroX, sdBuff[i].gyroY, sdBuff[i].gyroZ);
+            file.println(buf);
+          }
+          
+          file.close();
+          Serial.println("File closed!");
+
+          clearLCDY1();
+          M5.Lcd.drawCentreString("Write Complete!", LCDcenterX, LCDcenterY3, 2);
+          
+          //バッファ初期化
+          buffPointer = 0;
+          buffSaveFlg = false;
+          
+          // 計測開始
+          MyMenu.windowClr();
+          tickerSensor.attach_ms<MPU9250*>(16,_readSensor, &IMU);
+          tickerShowTime.attach_ms(1000, _showElapsedTime);
+          tickerWriteData.attach_ms(30000, _buffSave); 
+          M5.Lcd.drawCentreString("Measurement Start!", LCDcenterX, LCDcenterY1, 2);
+          Serial.println("All ticker attached!");
+        }
+        delay(100);
+      }
+      
+      //タイマーを止める
+      tickerSensor.detach();
+      tickerShowTime.detach();
+      tickerWriteData.detach();
+      //バッファ初期化
+      buffPointer = 0;
+      elapsedTime = 0;
+      buffSaveFlg = false;
+
+      MyMenu.windowClr();
+
+      // 加速度・ジャイロ保存終了
+      MyMenu.drawAppMenu(F("Save acc and gyro"),F("OK"),F("EXIT"),F("NEXT"));
+      MyMenu.showList();
+    }
+    M5.update();
+  }
+  free(sdBuff);
+  MyMenu.show();
 }
+
+
 
 // TODO
 // SD/accフォルダ内のCSVファイルの名前をリスト形式で羅列する。
@@ -323,7 +370,7 @@ int fileCount(fs::FS &fs, const char * dirname, uint8_t levels) {
             if(levels) {
               filecount += fileCount(fs, file.name(), levels -1);
             }
-         } else {
+        } else {
               filecount++;
             }
         file = root.openNextFile();
@@ -417,14 +464,14 @@ void _buffSave() {
 // ハンドラ3 (経過秒数の表示)
 void _showElapsedTime() {
   elapsedTime++;
-  clearY2;
+  clearLCDY2;
   M5.Lcd.drawCentreString((String(elapsedTime)+"seconds"), LCDcenterX, LCDcenterY2, 2);
 }
 
 // Y1, Y2, Y3それぞれの行のみをクリアするメソッド
-void clearY1() { M5.Lcd.fillRect(0, LCDcenterY1, 320, 20, MyMenu.getrgb(128, 128, 128)); }
-void clearY2() { M5.Lcd.fillRect(0, LCDcenterY2, 320, 20, MyMenu.getrgb(128, 128, 128)); }
-void clearY3() { M5.Lcd.fillRect(0, LCDcenterY3, 320, 20, MyMenu.getrgb(128, 128, 128)); }
+void clearLCDY1() { M5.Lcd.fillRect(0, LCDcenterY1, 320, 20, MyMenu.getrgb(128, 128, 128)); }
+void clearLCDY2() { M5.Lcd.fillRect(0, LCDcenterY2, 320, 20, MyMenu.getrgb(128, 128, 128)); }
+void clearLCDY3() { M5.Lcd.fillRect(0, LCDcenterY3, 320, 20, MyMenu.getrgb(128, 128, 128)); }
 
 // いろいろと試してみたいことをテストするメソッド。本機能には絡みません。
 void appSandBox(){
